673d789f7fc49d456ab03321df06beacf8b67761
Lee3qpqid3 <qdvbp5@gmail.com>
2026-04-19 00:08:01 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
